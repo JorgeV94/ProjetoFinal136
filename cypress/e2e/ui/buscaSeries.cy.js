c53b903e32e0327447084e1f388de7674eed5b3e
Jorge Vieira <jorge.vieira.a@hotmail.com>
2023-01-21 12:35:44 -0300

    Fim pagina series e inicio pagina shows

--- cypress/e2e/ui/buscaSeries.cy.js
+++ cypress/e2e/ui/buscaSeries.cy.js
@@ -12,21 +12,50 @@ describe('Pesquisas no site ', () => {
                     .type('The Librarians')
                 cy.get('button.medium.button.postfix')
                     .click()
-                cy.get('h1.small-12.cell')
+                cy.get('#search h1')
                     .should('have.text', 'Search')
                 cy.get('h3 a[href="/shows/340/the-librarians"]')
                     .click()
             })
 
-            it('Então é exibido no lado direito da página o campo "Show Info" \n E o campo "Network" com o texto/link "TNT" \n ', () => {
+            it('Então é exibido no lado direito da página o campo "Show Info" \n E o campo "Network" com o texto/link "TNT" \n E o campo "Official site" com o texto/link  "www.tntdrama.com" \n E o campo "Developed by" com o texto "John Rogers"', () => {
                 cy.get('#general-info-panel h2')
                     .should('have.text','Show Info')
                 cy.get('#general-info-panel div a')
                     .should('have.attr','href','/networks/14/tnt')
                 cy.get('#general-info-panel div a[href="/networks/14/tnt"]')
                     .should('have.text','TNT')
-                    
+                cy.get('#general-info-panel p a')  
+                    .should('have.text','www.tntdrama.com')
+                    .and('have.attr','href','http://www.tntdrama.com/shows/the-librarians.html')
+                cy.get('#general-info-panel div span a')
+                    .should('have.text','John Rogers')
             })
+        })
+
+        context('Quando inseri "Meryl Streep" no campo "pesquisa" \n E cliquei no botão "Search" fui redirecionado para a página de resultado de pesquisa', () => {
+            beforeEach(() => {
+                cy.get('#searchform-q')
+                    .type('Meryl Streep')
+                cy.get('button.medium.button.postfix')
+                    .click()
+                cy.get('#search h1')
+                    .should('have.text', 'Search')
+                cy.get('h3 a[href="/people/28226/meryl-streep"]')
+                    .click()
+            })
+            it('Então é exibido no lado esquerdo o titulo "Meryl Streep" \n E no lado direito da página as "Person Info" \n E os campos "Gender,Age,Birthday,Born in"', () => {
+                cy.get('.columns h1')
+                    .should('have.text','Meryl Streep')
+                cy.get('#general-info-panel h2')
+                    .should('have.text','Person Info')
+                cy.get('#general-info-panel p strong')
+                    .should('have.text','Gender:Age: Birthday:Born in:')
+                cy.get('#general-info-panel ul li')
+                    .should('have.text','Mary Louise Streep')
+
+            })
+            
         })
        
     })
---
+++ cypress/e2e/ui/paginaShows.cy.js
@@ -0,0 +1,36 @@
+describe('Pesquisas na pagina Show', () => {
+    
+    context('Dado que como visitante acessei a página SHOWS', () => {
+        beforeEach(() => {
+            cy.visit('/shows')
+        })
+
+        context('Visualizei a serie "NCIS" \n E cliquei no link \n E fui redirecionado para a página de Sinopse do titulo', () => {
+            beforeEach(() => {
+                cy.get('a[href="/shows/60/ncis"] img')
+                    .should('have.attr','src','https://static.tvmaze.com/uploads/images/medium_portrait/421/1053458.jpg')
+                    .click()
+                
+            })
+            it('Então visualizei a página de Sinopse verifiquei o titulo NCIS do lado esquerdo \n E é exibido no lado direito da página o titulo "Extra Details" \n E o subtitulo "Also known as:" ', () => {
+                cy.get('h1[class="show-for-medium"]')
+                    .should('have.text','NCIS') // div[class="callout generic-info-panel"] strong  Also known as: 
+                cy.get('#show [class="small-12 medium-4 cell"] h2')
+                    .contains('Extra Details')
+                cy.get('div[class="callout generic-info-panel"] strong')
+                    .should('have.text','Also known as: ') 
+                cy.get('[class="callout generic-info-panel"] li:first-child')
+                    .contains('Navy NCIS')
+                cy.get('[class="callout generic-info-panel"] li:nth-child(2)')
+                    .contains('NCIS: Investigações Criminais')
+                cy.get('[class="callout generic-info-panel"] li:nth-child(6)')
+                    .contains('NCIS rikostutkijat')
+                cy.get('[class="callout generic-info-panel"] li:nth-child(20)')
+                    .contains('Navy: investigación criminal')
+                cy.get('[class="callout generic-info-panel"] li:last-child')
+                    .contains('Морська полiцiя')
+            })
+        })
+       
+    })
+})
